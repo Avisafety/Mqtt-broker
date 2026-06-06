74a2719afc9fe464e686f7e2abc96b17862c95bb
Motionvr <hauggard@gmail.com>
2026-06-06 17:01:22 +0000

    Enable mosquitto debug logging

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,11 +3,9 @@ set -e
 
 mkdir -p /mosquitto/data
 
-if [ ! -f /mosquitto/data/passwd ]; then
-  mosquitto_passwd -b -c /mosquitto/data/passwd dji 'Test123456!'
-fi
+mosquitto_passwd -b -c /mosquitto/data/passwd dji 'Test123456!'
 
 chown -R mosquitto:mosquitto /mosquitto/data
-chmod 700 /mosquitto/data/passwd
+chmod 600 /mosquitto/data/passwd
 
 exec mosquitto -c /mosquitto/config/mosquitto.conf
