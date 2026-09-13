9b2f3d5239d095b2bb1f260d63ccd9f309c39a8c
Motionvr <hauggard@gmail.com>
2026-09-13 06:54:33 +0000

    Read MQTT credentials from Fly secrets instead of hardcoding

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 mkdir -p /mosquitto/data
 
-mosquitto_passwd -b -c /mosquitto/data/passwd dji 'Test123456!'
+mosquitto_passwd -b -c /mosquitto/data/passwd "$MQTT_USERNAME" "$MQTT_PASSWORD"
 
 chown -R mosquitto:mosquitto /mosquitto/data
 chmod 600 /mosquitto/data/passwd
